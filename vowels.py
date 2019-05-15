a5b5613dad3ee03eda2c03c0451886a08a293b89
nikrhem <50615701+nikrhem@users.noreply.github.com>
2019-05-15 11:25:11 +0530

    Add files via upload

---
+++ vowels.py
@@ -0,0 +1,13 @@
+alp = str(input())
+
+if((alp>='a'and alp<='z') or (alp>='A' and alp<='Z')):
+	if((alp=='a') or (alp=='e') or (alp=='i') or (alp=='o') or (alp=='u') or (alp=='A') or (alp=='E') or (alp=='I') or (alp=='O') or (alp=='U')):
+
+		print("Vowels")
+
+	else:
+
+		print("Consonants")
+
+else:
+	print("Invalid")
