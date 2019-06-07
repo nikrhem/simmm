890b0fdd5340b982e0daef9fe6afd9b679fe50aa
nikrhem <50615701+nikrhem@users.noreply.github.com>
2019-06-07 13:34:06 +0530

    Add files via upload

---
+++ longest_common_pref.py
@@ -0,0 +1,12 @@
+n=int(input())
+arr=[]
+for i in range(0,n):  
+    w=input()
+    arr.append(w)
+l=[]
+for i in zip(*arr):
+    if i.count(i[0])==len(i): 
+        l.append(i[0])
+    else:
+        break
+print(''.join(H))
