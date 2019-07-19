6d49b56907bb4584e28b49591c13be006ff5bf83
nikrhem <50615701+nikrhem@users.noreply.github.com>
2019-07-19 11:32:37 +0530

    Update subsequence.py

--- subsequence.py
+++ subsequence.py
@@ -1,13 +1,13 @@
-n = int(input())
-a = list(map(int,input().split()))
-p = 0
+nik = int(input())
+al = list(map(int,input().split()))
+pm = 0
 i=0
-while i<len(a)-1:
+while i<len(al)-1:
 	count = 0
-	while i<len(a)-1 and a[i]<a[i+1]:
+	while i<len(al)-1 and al[i]<al[i+1]:
 		count+=1
 		i+=1
-	if count>p:
-		p = count
+	if count>pm:
+		pm = count
 	i+=1
-print(p+1)
+print(pm+1)
