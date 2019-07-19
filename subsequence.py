0dcbc66e2dae6279cef5e263e177c8c8e63bc97e
nikrhem <50615701+nikrhem@users.noreply.github.com>
2019-07-19 11:26:12 +0530

    Add files via upload

---
+++ subsequence.py
@@ -0,0 +1,13 @@
+n = int(input())
+a = list(map(int,input().split()))
+p = 0
+i=0
+while i<len(a)-1:
+	count = 0
+	while i<len(a)-1 and a[i]<a[i+1]:
+		count+=1
+		i+=1
+	if count>p:
+			p = count
+	i+=1
+print(p+1)
